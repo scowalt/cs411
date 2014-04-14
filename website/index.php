376e587acd6fd86a48e295c502b793aef6d379f6
Scott Walters <srwalte2@illinois.edu>
2014-04-14 16:15:39 -0500

    Using Twig to render index page

--- website/index.php
+++ website/index.php
@@ -1,34 +1,27 @@
-<html>
-
-<body>
-<h1>Project Backend</h1>
-<p>A quest for food.</p>
-
-<form method="POST" action='/facility.php'>
-<select name='facility'>
-
-
 <?php
 /** Generate the facilities drop down selection menu **/
 
+// setup Twig
+require_once './vendor/autoload.php';
+$loader = new Twig_Loader_Filesystem('./views');
+$twig = new Twig_Environment($loader);
+
+// connect to the database
 $link = mysql_connect('engr-cpanel-mysql.engr.illinois.edu', 'cs411backend_web', 'teambackend');
 if (!$link) {
     die('Not connected : ' . mysql_error());
 }
 
+// query the database
 mysql_select_db('cs411backend_food', $link);
 $facility_names_query = "SELECT * FROM facilities";
 $result = mysql_query($facility_names_query) or die($facility_names_query. "<br/><br/>".mysql_error());;
 
+$rows = array();
 while(($row = mysql_fetch_row($result)) != null)
 {
-    echo "<option value='{$row[0]}'>{$row[1]}</option>";
+    array_push($rows, $row);
 }
+
+echo $twig->render('index.html', array('facilities' => $rows))
 ?>
-
-</select>
-<input type="submit" value="GO"/>
-</form>
-
-</body>
-</html>
