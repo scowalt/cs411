47765b2b35a545fb2c7f6a051b4231a5224770b0
Scott Walters <srwalte2@illinois.edu>
2014-04-15 14:21:33 -0500

    Automatically add new user for notification

--- website/notification.php
+++ website/notification.php
@@ -8,18 +8,24 @@ $twig = new Twig_Environment($loader);
 // connect to database
 $link = mysql_connect('engr-cpanel-mysql.engr.illinois.edu', 'cs411backend_web', 'teambackend');
 if (!$link) {
-    die('Not connected : ' . mysql_error());
+	die('Not connected : ' . mysql_error());
 }
 mysql_select_db('cs411backend_food', $link);
 
 // if the form has been submitted
 if(isset($_POST['submit'])){
+	// SQL escape data received
 	$netid = mysql_real_escape_string($_POST['netid']);
-    $facility = mysql_real_escape_string($_POST['facility']);
-    $item = mysql_real_escape_string($_POST['item']);
+	$facility = mysql_real_escape_string($_POST['facility']);
+	$item = mysql_real_escape_string($_POST['item']);
 
-    $query = "INSERT INTO notifications (user_net_id, food_name, facility_id) VALUES (\"$netid\",\"$item\",$facility)";
-    $result = mysql_query($query) or die($query . "<br/><br />" . mysql_error());;
+	// add user to database
+	$query = "INSERT IGNORE INTO users (user_net_id) VALUES (\"$netid\")";
+	$result = mysql_query($query) or die($query . "<br/><br />" . mysql_error());;
+
+	// add notification registration to database
+	$query = "INSERT INTO notifications (user_net_id, food_name, facility_id) VALUES (\"$netid\",\"$item\",$facility)";
+	$result = mysql_query($query) or die($query . "<br/><br />" . mysql_error());;
 
 	// display whether or not signing up for the notification was successful
 	echo "notification registration successful";
@@ -30,7 +36,7 @@ if(isset($_POST['submit'])){
 	$facilities = array();
 	while(($row = mysql_fetch_row($result)) != null)
 	{
-	    array_push($facilities, $row);
+		array_push($facilities, $row);
 	}
 
 	// query the database for food items
@@ -39,7 +45,7 @@ if(isset($_POST['submit'])){
 	$items = array();
 	while(($row = mysql_fetch_row($result)) != null)
 	{
-	    array_push($items, $row);
+		array_push($items, $row);
 	}
 
 	// display the notification sign-up form
