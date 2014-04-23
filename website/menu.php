ab9cb436527cd7c6499c301ddc5d65ed4e3435e6
Scott Walters <srwalte2@illinois.edu>
2014-04-23 18:01:14 -0500

    Added ratings stars (that don't do anything)
    Soon I will be able to show and update / remove user ratings with these
    stars. For no they're just kind of there

--- website/menu.php
+++ website/menu.php
@@ -16,14 +16,31 @@ if (!$link) {
 
 // query databaase
 mysql_select_db('cs411backend_food', $link);
-$menu_query = "SELECT * FROM menus_have_food_items WHERE menus_id = $menu_id";
+$menu_query = null;
+// if the user is logged in
+if (isset($_SESSION['user_email']) && $_SESSION['user_email']){
+	$netid = netidOf($_SESSION['user_email']);
+	// look for the ratings from that user
+	$menu_query = "SELECT food_items.food_name,rating FROM " . 
+		"menus_have_food_items NATURAL JOIN food_items LEFT JOIN ratings " .
+		"ON food_items.food_name = ratings.food_name " . 
+		"AND user_net_id = \"$netid\" WHERE menus_id = $menu_id";
+} else {
+	// just look for the food items
+	$menu_query = "SELECT food_name FROM menus_have_food_items WHERE menus_id = $menu_id";
+}
+
 $result = mysql_query($menu_query) or die($menu_query. "<br/><br/>".mysql_error());
 
-$rows = array();
+$food_items = array();
 while(($row = mysql_fetch_row($result)) != null)
 {
-	array_push($rows, $row);
+	array_push($food_items, $row);
 }
-echo $twig->render('menu.html', array('items' => $rows, 'user' => $_SESSION['user_email']));
+echo $twig->render('menu.html', array('items' => $food_items, 'user' => $_SESSION['user_email']));
+
+function netidOf($email){
+	return substr($email, 0, (strlen($email) - 13));
+}
 
 ?>
