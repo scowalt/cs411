2cb64bf0e02014d374265665a57284f274396070
Scott Walters <srwalte2@illinois.edu>
2014-04-23 18:39:33 -0500

    More work on food rating code

---
+++ website/ratings/remove.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+if (!isset($_POST['item']))
+	die;
+
+// connect to the database
+$link = mysql_connect('engr-cpanel-mysql.engr.illinois.edu', 'cs411backend_web', 'teambackend');
+if (!$link) {
+    die('Not connected : ' . mysql_error());
+}
+mysql_select_db('cs411backend_food', $link);
+
+$netid = mysql_real_escape_string(netidOf($_SESSION['user_email']));
+$item = mysql_real_escape_string($_POST['item']);
+
+// query the database
+$query = "DELETE from ratings " .
+	"WHERE user_net_id = $netid AND food_name = $item";
+$result = mysql_query($query) or die($query . "<br/><br />" . mysql_error());;
+
+function netidOf($email){
+	return substr($email, 0, (strlen($email) - 13));
+}
+
+?>
